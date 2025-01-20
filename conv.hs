6a6a081bdcd52c42cf68e5d29847be50f0385a57
Shameena Malik <shameenamalik@duck.com>
2025-01-20 15:57:35 +0000

    add converter from Lambda to Comb

--- conv.hs
+++ conv.hs
@@ -1,24 +1,27 @@
 module Conv where
 
 import Parser
+import Comb
 import Lexer
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Control.Monad
 
 
-data Lambda = Var String | F String Lambda | Ap Lambda Lambda 
+-- Program -> Lambda
+
+data Lambda = Var String | F String Lambda | Ap Lambda Lambda deriving Eq
 
 instance Show Lambda where
   show (Var s) = s
   show (F s l) = "\\" ++ s ++ "." ++ show l
   show (Ap x y) = wrap (show x) ++ wrap (show y) where wrap s = if (length s == 1) then s else "(" ++ s ++ ")"
 
-
 conv_to_lambda :: Map String Lambda_term -> [Line] -> [Lambda_term]
 conv_to_lambda _ [] = [] 
 conv_to_lambda dict (l:ls) = 
   case l of (Assign s f) -> conv_to_lambda (Map.insert s f dict) ls
-            (Run x)      -> (conv_term x dict) : conv_to_lambda dict ls
+            (RunTerm x)  -> (conv_term x dict) : conv_to_lambda dict ls
 
 
 conv_term :: Lambda_term -> Map String Lambda_term -> Lambda_term
@@ -44,6 +47,56 @@ num_to_lambda :: Int -> Lambda
 num_to_lambda 0 = Var "x"
 num_to_lambda n = Ap (Var "f") (num_to_lambda $ n-1)
 
-run = (map undo_shorthand) . (conv_to_lambda Map.empty) . parser . lexer
+-- Lambda -> SK 
 
--- TODO: Lambda -> SK 
+-- rules:
+-- \\x.C = KC
+-- \\x.x = SKK
+-- \\x.AB = S (\\x.A) (\\x.B)
+
+
+lambda_to_comb :: Lambda -> Comb
+
+lambda_to_comb l =
+  case l of (F x y)  -> lambda_to_comb' x (lambda_to_comb y)
+            (Ap x y) -> App (lambda_to_comb x) (lambda_to_comb y) 
+            (Var x)  -> V x
+
+lambda_to_comb' :: String -> Comb -> Comb 
+lambda_to_comb' x y =
+  case y of (App a b) -> App (App S (lambda_to_comb' x a)) (lambda_to_comb' x b)
+            otherwise -> if y == (V x) then App (App S K) K else App K y
+
+run_lambda = (map undo_shorthand) . (conv_to_lambda Map.empty) . parser . lexer
+run = (map lambda_to_comb) . (map undo_shorthand) . (conv_to_lambda Map.empty) . parser . lexer
+run_single x = (run x) !! 0
+
+-- for testing
+c_id = (App (App S K) K)
+one = (S `App` ((S `App` (K `App` S)) `App` ((S `App` (K `App` K)) `App` c_id))) `App` (K `App` c_id)
+fx f = (App (App f (V "f")) (V "x"))
+
+
+numeral :: Int -> Comb
+numeral 1 = run_single "run \\f x. f x"
+numeral n = plusOne (numeral $ n-1)
+
+
+plusOne :: Comb -> Comb
+plusOne n = let p = run_single "run \\m. \\f x. f (m f x)"
+                in App p n
+
+-- isNumeral :: Comb -> Maybe Int
+-- isNumeral c = isNumeralGreaterThan c 0
+
+-- isNumeralGreaterThan :: Comb -> Int -> Maybe Int
+-- isNumeralGreaterThan c n = if c_length c < c_length (numeral n) 
+                           -- then Nothing 
+                           -- else if c == numeral n 
+                                -- then Just n 
+                                -- else isNumeralGreaterThan c (n+1)
+
+-- c_length :: Comb -> Int
+-- c_length S = 1
+-- c_length K = 1
+-- c_length (App x y) = c_length x + c_length y
