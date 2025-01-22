5bd6f3db1d8c15f584b90fdb5112e44bc894a438
Shameena Malik <shameenamalik@duck.com>
2025-01-22 20:31:58 +0000

    recognise some forms of numbers when printing Combs

--- comb.hs
+++ comb.hs
@@ -1,6 +1,8 @@
 module Comb where
 
 import Control.Monad
+import Data.List
+import Data.Char
 
 data Comb = S | K | V String | App Comb Comb deriving Eq
 
@@ -9,7 +11,13 @@ instance Show Comb where
   show S = "S"
   show K = "K"
   show (V s) = s
-  show (App x y) = wrap (show x) ++ wrap (show y) where wrap s = if (length s == 1) then s else "(" ++ s ++ ")"
+  show (App x y) =
+    case is_num (App x y) of (Just n) -> show n 
+                             Nothing  -> let sx = show x; sy = show y
+                                             wrap s = if (length s == 1) then s else "(" ++ s ++ ")"
+                                             is_num = (foldr (&&) True ). map isDigit
+                                         in if (is_num sx && is_num sy) then "(" ++ sx ++ ")" ++ sy
+                                            else wrap sx ++ wrap sy										 
 
 data Run a = Run a 
 
@@ -63,3 +71,33 @@ annotated_run_comb x = m_run_comb (\c -> Log [show c] c) x
 
 show_annotated (Log s x) = mapM_ putStrLn (s ++ [show x])
 log_run_comb = show_annotated . annotated_run_comb 
+
+run_c :: Comb -> Comb
+run_c (App (App K a) b) = run_c a
+run_c (App (App (App S f) g) x) = run_c (App (App f x) (App g x))
+run_c (App x y) = let x' = run_c x
+                      y' = run_c y
+                  in if (x == x' && y == y') then App x y else run_c (App x' y')
+run_c x = x
+   
+c_length :: Comb -> Int
+c_length S = 1
+c_length K = 1
+c_length (V x) = 1
+c_length (App x y) = c_length x + c_length y
+													   
+c_zero = App (App S (App (App S (App K S)) (App K K))) (App K K)
+c_plus_one = App S (App (App S (App K S)) (App (App S (App K K)) (App (App S K) K) ))
+
+c_num :: Int -> Comb
+c_num 0 = c_zero
+c_num n = App c_plus_one (c_num $ n-1)
+
+is_num :: Comb -> Maybe Int 
+is_num c = is_num' c 0
+
+is_num' :: Comb -> Int -> Maybe Int
+is_num' c n = let x = c_num n
+              in if c_length c < c_length x then Nothing
+                 else if c == x then Just n
+                      else is_num' c (n+1)
