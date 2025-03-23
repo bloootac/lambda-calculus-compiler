469593e1e93ab682520ac441dc62f515fa8908d9
Asfur Malik <shameenamalik@duck.com>
2025-03-23 18:08:58 +0000

    convert file text to Comb tree structure

--- runc.c
+++ runc.c
@@ -3,29 +3,122 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <stdbool.h>
+#include "runc.h"
+
+/*
+TODO:
+ - conv from str to tree struct [DONE] (i think!)
+ - look for reductions in tree struct <-
+ - and perform reduction on struct
+*/
 
 void main() {
-	//try to open file
-	const char* filename = "program.sk";
-	FILE *fptr = fopen(filename, "r");
+    //try to open file
+    const char* filename = "program.sk";
+    FILE *fptr = fopen(filename, "r");
+    
+    //exit(0) = success, exit(1) = failure
+    if (!fptr) exit(1);
+    
+    //tries to store file info in sb. need this to get the file size
+    struct stat sb;
+    if (stat(filename, &sb) == -1) exit(1);
+    
+    //allocate enough space to store input, then read from file
+    char* input = malloc(sb.st_size + 1);
+    fread(input, sb.st_size, 1, fptr);
+    input[sb.st_size] = '\0';
 	
-	//exit(0) = success, exit(1) = failure
-	if (!fptr) exit(1);
+    //print input 
+    printf("%s\n", input);
 	
-	//tries to store file info in sb. need this to get the file size
-	struct stat sb;
-	if (stat(filename, &sb) == -1) exit(1);
+	//initialise struct
+	Comb* root = malloc(sizeof(Comb));
 	
-	//allocate enough space to store input, then read from file
-	char* input = malloc(sb.st_size);
-	fread(input, sb.st_size, 1, fptr);
+	strToTree(root, &(root->left), &(root->right), &(root->val), input);
 	
-	//print input (sometimes adds an i at the end??)
-	printf("%s", input);
+	printTree(root);
+    //close file, de-allocate memory
+    fclose(fptr);
+    free(input);
+    
+    exit(0); 
+}
+
+void strToTree(Comb* comb, Comb** left, Comb** right, char** val, char* str) {
 	
-	//close file, de-allocate memory
-	fclose(fptr);
-	free(input);
+	if (!*str) {
+		printf("called strToTree on null string"); // ಠ_ಠ
+		exit(1);
+	}
 	
-	exit(0); 
+	char* splitIndex;
+	
+	if (*str == '+') {	
+		//allocate memory for left + right terms
+		*left = malloc(sizeof(Comb));
+		*right = malloc(sizeof(Comb));
+		
+		//str = +AB. find index so we can separate A and B
+		splitIndex = splitCombStr(str);
+		
+		//left = A
+		strToTree(*left, &((*left)->left), &((*left)->right), &((*left)->val), str + 1);
+		//right = B
+		strToTree(*right, &((*right)->left), &((*right)->right), &((*right)->val), splitIndex);
+		
+	}
+	else { 
+		//str = S, K, or (var). find index to get full variable name
+		splitIndex = getVar(str);
+		
+		//copy var to node 
+		*val = malloc(splitIndex - str + 1); 
+		memcpy(*val, str, splitIndex - str);
+		*(*val + (splitIndex - str)) = '\0';
+		
+		//so we know this node is a leaf
+		*left = NULL; *right = NULL;
+	}
+	
+}
+
+
+//input: +AB. returns pointer to 1st char of B
+char* splitCombStr(char *str) {
+	int count = 0;
+
+	while (*str) {
+		if (*str == '+') count++;
+		else if (*str == ')' || *str == 'S' || *str == 'K') count--;
+		str++;
+		if (count == 0) break;
+	}
+	return str;
+	
+}
+
+//input: (var), S, or K. returns pointer to char after end of variable
+char* getVar(char *str) {
+	if (*str != '(') return str + 1;
+	while (*str && *str != ')') str++;
+	return str + 1;
+}
+
+void printTree(Comb *comb) {
+	
+	if (comb != NULL) {
+		
+		if (comb->left == NULL)  {
+			printf("%s", comb->val); 
+		}
+		else {
+			printf("+");
+			printTree(comb->left);
+			printTree(comb->right);
+		}
+		
+	}
+	
 }
